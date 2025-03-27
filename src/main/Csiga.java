eb5ea91df376374dbf93e9dcd299387a1759aaf9
KiraLujza <kira.lujza@gmail.com>
2025-03-27 15:39:51 +0100

    Csiga Class kész

--- src/main/Csiga.java
+++ src/main/Csiga.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
 
-/**
- *
- * @author KissKiraLujza(Szf_N_
- */
+import java.util.Random;
+
 public class Csiga {
+
+    Random RND = new Random();
+
+    private String szin;
+    private String szinkód;
+    private int sebesseg;
+    private final String TAV = "-";
+    private final String GYTAV = "=";
+    private boolean gyors;
+
+    public Csiga(String szin) {
+        this.szin = szin;
+        this.gyors = false;
+    }
     
+    public int SebessegSorsolas(){
+        this.sebesseg = RND.nextInt(4);
+        return sebesseg;
+    }
+
+    public String getSzinKód() {
+        return szinkód;
+    }
+    
+    public void setSzinkód(String szinkód){
+        this.szinkód = szinkód;
+    }
+    public void setGyors(){
+         this.gyors = true;
+    }
+    
+    public String getTav(){
+        return this.TAV;
+    }
+    
+    public String getGyTav(){
+        return this.GYTAV;
+    }
 }
